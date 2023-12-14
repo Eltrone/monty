4f6f934ff82b37217521463bb569ddb596b45f36
Eltrone <amineelorche@gmail.com>
2023-12-14 11:20:28 -0800

    script added

---
+++ test.m
@@ -0,0 +1,41 @@
+# Test push and pall
+push 1
+push 2
+push 3
+pall
+
+# Test pint
+pint
+
+# Test pop
+pop
+pall
+pop
+pall
+
+# Test swap
+push 4
+push 5
+pall
+swap
+pall
+
+# Test add
+push 6
+add
+pall
+
+# Test nop
+nop
+pall
+
+# Test error handling
+# Uncomment the following lines to test error handling
+# push a
+# pall
+# pop
+# pop
+# swap
+# add
+# pint
+
