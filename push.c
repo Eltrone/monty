c502235dac5f3138d6b36bfbeccacae5958899f5
Eltrone <amineelorche@gmail.com>
2023-12-14 11:22:09 -0800

    script added

--- push.c
+++ push.c
@@ -7,40 +7,40 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	tab_s *new_node;
-	char *arg;
-	int num;
+    stack_t *new_node;
+    char *arg;
+    int num;
 
-	/* Obtention de l'argument pour push */
-	arg = strtok(NULL, " \n\t\r");
-	if (arg == NULL || !is_number(arg))
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+    /* Obtention de l'argument pour push */
+    arg = strtok(NULL, " \n\t\r");
+    if (arg == NULL || !is_number(arg))
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-	/* Conversion de l'argument en entier */
-	num = atoi(arg);
+    /* Conversion de l'argument en entier */
+    num = atoi(arg);
 
-	/* Création d'un nouveau nœud */
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
+    /* Création d'un nouveau nœud */
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
 
-	/* Initialisation du nouveau nœud */
-	new_node->n = num;
-	new_node->prev = NULL;
-	new_node->next = *stack;
+    /* Initialisation du nouveau nœud */
+    new_node->n = num;
+    new_node->prev = NULL;
+    new_node->next = *stack;
 
-	/* Mise à jour de l'ancien sommet de la pile */
-	if (*stack != NULL)
-		(*stack)->prev = new_node;
+    /* Mise à jour de l'ancien sommet de la pile */
+    if (*stack != NULL)
+        (*stack)->prev = new_node;
 
-	/* Mise à jour du sommet de la pile */
-	*stack = new_node;
+    /* Mise à jour du sommet de la pile */
+    *stack = new_node;
 }
 
 /**
@@ -50,15 +50,15 @@ void push(stack_t **stack, unsigned int line_number)
  */
 int is_number(char *str)
 {
-	if (*str == '-' || *str == '+')
-		str++;
+    if (*str == '-' || *str == '+')
+        str++;
 
-	while (*str)
-	{
-		if (*str < '0' || *str > '9')
-			return (0);
-		str++;
-	}
+    while (*str)
+    {
+        if (*str < '0' || *str > '9')
+            return (0);
+        str++;
+    }
 
-	return (1);
+    return (1);
 }
